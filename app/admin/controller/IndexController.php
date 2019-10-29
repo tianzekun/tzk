9e9e6feba1dd49504fe3c221744c89e67ba1de5b
于破熊 <8553151@qq.com>
2019-10-29 17:14:43 +0800

    创建基本模块

---
+++ app/admin/controller/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\admin\controller;
+
+class IndexController extends Controller
+{
+
+}
---
+++ app/admin/service/AdminUserService.php
@@ -0,0 +1,14 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\admin\service;
+
+
+class AdminUserService
+{
+
+    public function login()
+    {
+
+    }
+}
